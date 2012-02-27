1428c4efd334575d4d6e0ed2af88d9ec2401e545
Twisted <twistedumbrella@gmail.com>
2012-02-26 22:26:25 -0500

    Copy and paste do not exist in this dojo
    Kangs do not exist in this dojo

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,5 +20,5 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo cm_droid2-userdebug
-add_lunch_combo cm_droid2-eng
+add_lunch_combo moto_droid2-userdebug
+add_lunch_combo moto_droid2-eng
