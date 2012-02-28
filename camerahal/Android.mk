5a16c8d64533bee22ff47c80ec5b9fd2201840e0
Twisted <twistedumbrella@gmail.com>
2012-02-28 11:52:16 -0500

    Rename directories for content specification

--- camerahal/Android.mk
+++ camerahal/Android.mk
@@ -1,14 +1,13 @@
-ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),droid2)
-
 LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_MOTO_CAMERA),true)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE_TAGS    := optional
 LOCAL_MODULE_PATH    := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-LOCAL_MODULE         := camera.droid2
+LOCAL_MODULE         := camera.$(TARGET_BOARD_PLATFORM)
 LOCAL_SRC_FILES      := cameraHal.cpp
-LOCAL_PRELINK_MODULE := false
+LOCAL_MODULE_TAGS    := optional
 
 LOCAL_SRC_FILES += v4l2_utils.c overlay.cpp
 
@@ -20,10 +19,14 @@ LOCAL_SHARED_LIBRARIES += \
     libmedia \
     libhardware \
     libcamera_client \
-    libcamera \
+    libcamera
 
 LOCAL_SHARED_LIBRARIES += libdl
 
 include $(BUILD_SHARED_LIBRARY)
+
+ifeq ($(OMAP_ENHANCEMENT),true)
+LOCAL_CFLAGS += -DOMAP_ENHANCEMENT
+endif
 
 endif
